3d67e3b27fbf5dd8ad08f6a775e97d3f9c7673a9
Stefy <stefy.kris14@gmail.com>
2026-02-26 20:43:59 -0500

    Agregar archivos de proyecto.

---
+++ prueba1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tarifaPorHora = 1.50;
+            double horas;
+            double total;
+
+            Console.WriteLine("===== SISTEMA DE PARQUEO =====");
+
+            Console.Write("Ingrese hora de entrada (ejemplo 8.5 = 8:30): ");
+            double horaEntrada = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese hora de salida (ejemplo 14.5 = 14:30): ");
+
+            double horaSalida = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
